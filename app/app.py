4c59740ced6c19e0854044b35f0839ea00e219ef
Mayukh <banerjeechandrani506@gmail.com>
2025-11-12 16:41:14 +0530

    Refreshh

--- app/app.py
+++ app/app.py
@@ -1,44 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from app.schemas import PostCreate
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-this_app = FastAPI()
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+   await create_db_and_tables()
+   yield
 
-text_posts = {
-    1: {"title": "First Post", "Desc": "Gud yaar"},
-    2: {"title": "Second Post", "Desc": "Bhai scene on hai"},
-    3: {"title": "Third Post", "Desc": "Kal milte hai"},
-    4: {"title": "Fourth Post", "Desc": "Coding vibes"},
-    5: {"title": "Fifth Post", "Desc": "Aaj mood mast"},
-    6: {"title": "Sixth Post", "Desc": "Life set"},
-    7: {"title": "Seventh Post", "Desc": "Kya bolti public"},
-    8: {"title": "Eighth Post", "Desc": "Chai break"},
-    9: {"title": "Ninth Post", "Desc": "Thoda kaam karo"},
-    10: {"title": "Tenth Post", "Desc": "Sona zaroori hai"},
-}
+app = FastAPI(lifespan = lifespan)
 
 
-@this_app.get("/posts")# ? -> denotes the query parameters
-def get_all_posts(lim : int):
-   if lim:
-      return list(text_posts.values())[:lim]
-
-
-# Get operation with custom path parameter 
-@this_app.get("/posts/{id}")
-def get_post(id: int):
-   if id not in text_posts:
-      raise HTTPException(status_code=403, detail=  "Post not found!!!!")
-   return text_posts.get(id)
-
-@this_app.post("/posts")
-def create_post(post : PostCreate):
-   new_post = {
-      "title": post.title,
-      "Desc": post.Desc
-   }
-   text_posts[max(text_posts.keys()) + 1] = new_post
-   return new_post
-
-
-# @this_app.delete -> later 
    
--- app/db.py
+++ app/db.py
@@ -5,12 +5,14 @@ from sqlalchemy import Column, String , Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
-import datetime
+from datetime import timezone
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
+class Base(DeclarativeBase):
+    pass
 
-class Post(DeclarativeBase):
+class Post(Base):
     __tablename__ = "posts"
     
     #ID of the the post..which is guranteed to be unique
@@ -19,7 +21,7 @@ class Post(DeclarativeBase):
     url = Column(String, nullable = False)
     file_type = Column(String, nullable = False)
     file_name = Column(String, nullable = False)
-    created_at = Column(DateTime, default = datetime.utcnow)
+    created_at = Column(DateTime, default = timezone.utc)
     
     
 engine = create_async_engine(DATABASE_URL)
@@ -27,7 +29,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit = False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
         
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
