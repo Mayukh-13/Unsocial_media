8192dbb9c03b26d3f72f8a255a33a566a257a20a
Mayukh <banerjeechandrani506@gmail.com>
2025-11-12 17:38:29 +0530

    feed implemented

--- app/app.py
+++ app/app.py
@@ -1,15 +1,57 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app : FastAPI):
    await create_db_and_tables()
    yield
 
-app = FastAPI(lifespan = lifespan)
+app1 = FastAPI(lifespan = lifespan)
+
+
+@app1.post("/upload")
+async def upload_file(
+   file: UploadFile = File(...),
+   caption: str = Form(""),
+   session: AsyncSession = Depends(get_async_session)
+):
+   post = Post(
+      caption = caption,
+      url = "dummy url",
+      file_type = "photo",
+      file_name = "dummy name"
+   )
+   session.add(post)
+   await session.commit()#saves it
+   await session.refresh(post)
+   return post
+
+@app1.get("/feed")
+async def get_feed(
+   session: AsyncSession = Depends(get_async_session)
+):
+   result = await session.execute(select(Post).order_by(Post.created_at.desc())) # look up sql alchemy for filters etc
+   posts = [row[0] for row in result.all()]
+   posts_data = []
+   for post in posts:
+      posts_data.append(
+         {
+            "id": str(post.id),
+            "caption": post.caption,
+            "url": post.url,
+            "file_type": post.file_type,
+            "file_name": post.file_name,
+            "created_at": post.created_at.isoformat()
+         }
+      )
+   
+   return posts_data
+   
+
 
 
    
